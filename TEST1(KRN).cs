90e88a14796ce3fca03898f4c692410248b7e17d
pongsakorn <78690934+pongsakornsutana@users.noreply.github.com>
2021-03-28 18:38:45 +0700

    Add files via upload

---
+++ TEST1(KRN).cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TTTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ข้อ2คูณร่วมน้อย
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x=0;
+            int y=0;
+            x = a;
+            y = b;
+
+            while (x != y)
+                    
+            {
+                if (x < y)
+                {
+                    x = x + a;
+
+                }
+                else
+                {
+                    y = y + b;
+                }  
+            }
+            Console.Write("{0}", x);
+           
+        }
+    }
+}
+
